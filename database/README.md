1fbc41213478ce990cc7b27203d2c85d295e1f66
SvenLieber <sven.lieber@ugent.be>
2021-08-19 09:45:55 -0600

    added some Blazegraph utils scripts
    creating namespaces via Blazegraph's REST API

---
+++ database/README.md
@@ -0,0 +1,4 @@
+# Database
+
+The mapping creates the Knowledge Graph and can be stored as RDF files, but within the mapings also a SPARQL endpoint can be specified.
+This folder contains software to start and prepare a Blazegraph triple store.
---
+++ database/create-namespace.sh
@@ -0,0 +1,20 @@
+if [ $# -ne 1 ];
+then
+  echo "Please provide an import config file"
+  exit 1
+fi;
+
+if [ ! -f $1 ];
+then
+  echo "The provided name is not a valid file";
+  exit 1
+fi;
+
+# get environment variables
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+curl -X POST \
+#  --user $SPARQL_ENDPOINT_USER:$SPARQL_ENDPOINT_PASSWORD \
+  --data-binary @$1 \
+  --header 'Content-Type:application/xml' \
+  $SPARQL_ENDPOINT_URL/bigdata/namespace
